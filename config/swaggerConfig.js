2d8cebc4e902da6e35bc097535ec095d483a92e7
laismicroni <laismicroni@gmail.com>
2024-05-13 14:18:38 -0300

    imp swagger ui

--- config/swaggerConfig.js
+++ config/swaggerConfig.js
@@ -1,65 +1,7 @@
-const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Fleet Management API',
-      version: '1.0.0',
-      description: 'Documentação da API REST para o projeto de taxis',
-    },
-    components: {
-      schemas: {
-        taxis: {
-          type: 'object',
-          properties: {
-            id: {
-              type: 'integer',
-              description: 'ID do táxi',
-            },
-            plate: {
-              type: 'string',
-              description: 'Placa do táxi',
-            },
-          },
-        },
-        trajectories: {
-          type: 'object',
-          properties: {
-            id: {
-              type: 'integer',
-              description: 'ID da trajetória',
-            },
-            date: {
-              type: 'string',
-              format: 'date-time',
-              description: 'Data da trajetória',
-            },
-            latitude: {
-              type: 'number',
-              format: 'float',
-              description: 'Latitude da trajetória',
-            },
-            longitude: {
-              type: 'number',
-              format: 'float',
-              description: 'Longitude da trajetória',
-            },
-            taxi_id: {
-              type: 'integer',
-              description: 'ID do táxi associado a esta trajetória',
-            },
-          },
-        },
-      },
-    },
-  },
-  apis: ['./routes/*.js'],
-};
-
-const swaggerSpec = swaggerJsdoc(options);
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 module.exports = function configureSwagger(app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
--- routes/taxis.js
+++ routes/taxis.js
@@ -2,54 +2,6 @@ const express = require('express');
 const router = express.Router();
 const taxisController = require('../controllers/taxisController');
 
-/**
- * @swagger
- * tags:
- *   name: Taxis
- *   description: Operações relacionadas aos táxis
- */
-
-/**
- * @swagger
- * /api/taxis:
- *   get:
- *     summary: Listar todos os táxis
- *     description: Retorna uma lista paginada de todos os táxis.
- *     tags: [Táxis]
- *     parameters:
- *       - in: query
- *         name: page
- *         description: Número da página a ser recuperada
- *         schema:
- *           type: integer
- *           default: 1
- *       - in: query
- *         name: limit
- *         description: Número máximo de registros por página
- *         schema:
- *           type: integer
- *           default: 10
- *     responses:
- *       200:
- *         description: Lista paginada de táxis
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 totalPages:
- *                   type: integer
- *                   description: Número total de páginas
- *                 currentPage:
- *                   type: integer
- *                   description: Número da página atual
- *                 taxis:
- *                   type: array
- *                   items:
- *                     $ref: '#/components/schemas/taxis'
- *       500:
- *         description: Erro interno do servidor
- */
 router.get('/taxis', taxisController.listAllTaxis);
 
 module.exports = router;
