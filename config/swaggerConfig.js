328da468327060b7a1882ec43b238121db742900
laismicroni <laismicroni@gmail.com>
2024-05-14 12:08:35 -0300

    swagger path

--- config/swaggerConfig.js
+++ config/swaggerConfig.js
@@ -1,6 +1,10 @@
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const path = require('path');
+
+const swaggerPath = path.resolve(__dirname, '../swagger.yaml');
+
+const swaggerDocument = YAML.load(swaggerPath);
 
 module.exports = function configureSwagger(app) {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
