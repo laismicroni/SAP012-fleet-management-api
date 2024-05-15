fb69524c83774a4bdf6052236f2e14af5c0a56b4
laismicroni <laismicroni@gmail.com>
2024-05-15 15:21:14 -0300

    swagger webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  
+
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        './node_modules/swagger-ui-dist/swagger-ui.css',
+        './node_modules/swagger-ui-dist/swagger-ui-bundle.js',
+        './node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js',
+        './node_modules/swagger-ui-dist/favicon-16x16.png',
+        './node_modules/swagger-ui-dist/favicon-32x32.png'
+      ]
+    })
+  ]
+};
