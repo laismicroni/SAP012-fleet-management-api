9c5378e101038418e97c15f60e82a0c29d1ed31a
laismicroni <laismicroni@gmail.com>
2024-05-15 11:52:56 -0300

    swagger test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const swaggerConfig = require('./config/swaggerConfig');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json'); 
 const taxisRoutes = require('./routes/taxis');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-// Configurar o Swagger
-swaggerConfig(app);
+// Configurar o Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Adicionar as rotas
 app.use('/api', taxisRoutes);
